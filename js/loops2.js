31be188244853e116514edd5ba1579eb0bfefc5f
Beefaz <justinas.geryba@gmail.com>
2024-02-01 18:32:28 +0200

    added finished loops2

--- js/loops2.js
+++ js/loops2.js
@@ -5,9 +5,9 @@ const rand = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 document.write('<h5>4. Arrays2</h5><br/>');
+document.write('<br/><br/>');
 // ------------TASKS------------------
 // 1.
-document.write('<br/><br/>');
 
 document.write('1. Užduotis ' + '<br/><br/>');
 const printRange = () => {
