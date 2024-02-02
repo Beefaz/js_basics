2b1af81650aa096a47fc36c0135a0ca2dbb11483
Beefaz <justinas.geryba@gmail.com>
2024-02-02 19:44:40 +0200

    added finished loops2

--- js/arrays.js
+++ js/arrays.js
@@ -124,11 +124,67 @@ document.write('<br/><br/>');
 document.write('2.i ');
 let removedSecondaryElements = [];
 for (let i = 1; i < defaultArray.length; i += 2) {
-    removedSecondaryElements.push(defaultArray[i]);
+  removedSecondaryElements.push(defaultArray[i]);
 }
-
 document.write(`Remaining array w/o secondary indexes: ${removedSecondaryElements}`);
 document.write('<br/><br/>');
+
+
+// 3.
+document.write('3. ');
+const array1 = [];
+const array2 = [];
+while (array1.length < 100 && array2.length < 100) {
+  const tempNumber = rand(100, 999);
+  const tempNumber2 = rand(100, 999);
+  if (!array1.includes(tempNumber) && array1.length < 100) {
+    array1.push(tempNumber);
+  }
+  if (!array2.includes(tempNumber2) && array2.length < 100) {
+    array2.push(tempNumber2);
+  }
+}
+document.write('<br/>');
+document.write(`Array1: ${array1}`);
+document.write('<br/>');
+document.write(`Array2: ${array2}`);
+document.write('<br/><br/>');
+
+// 4.
+document.write('4. ');
+const newArrayXX = [];
+
+for (let i = 0; i < array1.length; i++) {
+  if (!array2.includes(array1[i])) {
+    newArrayXX.push(array1[i]);
+  }
+}
+document.write(`Non dublicating numbers: ${newArrayXX}`);
+document.write('<br/><br/>');
+
+// 5.
+document.write('5. ');
+const newArrayXXX = [];
+
+for (let i = 0; i < array1.length; i++) {
+  if (array2.includes(array1[i])) {
+    newArrayXXX.push(array1[i]);
+  }
+}
+document.write(`Dublicating numbers: ${newArrayXXX}`);
+document.write('<br/><br/>');
+
+
+// 6.
+document.write('6. ');
+const tenNumbers = [rand(5, 25), rand(5, 25)];
+for (i = 2; i < 10; i++) {
+  tenNumbers[tenNumbers.length] = tenNumbers[i - 2] + tenNumbers[i - 1];
+}
+
+document.write(`${tenNumbers}`);
+document.write('<br/><br/>');
+
 document.write('<br/><br/><a href="/tasks/5Masyvai.pdf">Užduotis</a>');
 
 
