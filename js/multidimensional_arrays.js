19c4212a74c61d5a133ea68f6ee8673ec920a139
Beefaz <justinas.geryba@gmail.com>
2024-02-09 14:41:04 +0200

    task finished

--- js/multidimensional_arrays.js
+++ js/multidimensional_arrays.js
@@ -83,8 +83,6 @@ console.log('---------------------');
 
 console.log('2e. ');
 const sumChildValues = (arrayWithChildArrays) => {
-  console.log(arrayWithChildArrays);
-
   let equalIndexSums = [];
   equalIndexSums.length = arrayWithChildArrays[0].length;
 
@@ -123,9 +121,67 @@ console.log('---------------------');
 console.log('4. ');
 const sort2ndDimension = (arrayWithChildArrays, letter = 'K') => {
   const sortedArray = [...arrayWithChildArrays];
-  sortedArray.sort((a, b) => (a.length - b.length)).sort((a) => a.includes(letter) ? -1 : 1)
+  sortedArray.sort((a, b) => (a.length - b.length)).sort((a, b) => (a.includes(letter) ? -1 : 1) - (b.includes(letter) ? -1 : 1));
   return sortedArray;
 }
 console.log(sort2ndDimension(generate2dLetterArray(), 'K'));
 console.log('---------------------');
+
+
+console.log('5. ');
+const generate2dArrayX = () => {
+  const firstLevelArray = [];
+  for (let i = 0; i < 10; i++) {
+    const randomLength = rand(0, 5);
+
+    if (randomLength === 0) {
+      firstLevelArray[i] = rand(0, 10);
+    } else {
+      const randomLength = rand(0, 10);
+      firstLevelArray[i] = [];
+
+      for (let ii = 0; ii < randomLength; ii++) {
+        firstLevelArray[i][ii] = rand(0, 10);
+      }
+    }
+  }
+  return [...firstLevelArray];
+}
+
+console.log(generate2dArrayX());
+console.log('---------------------');
+
+console.log('6. ');
+const calculateSumsOfTask5 = (arrayToCalc) => {
+  return arrayToCalc.flat(2).reduce((sum, val) => sum + val, 0);
+}
+console.log(calculateSumsOfTask5(generate2dArrayX()));
+
+const sortArrayOfTask5 = (arrayToSort) => {
+  return arrayToSort.sort((a, b) => {
+    if (Array.isArray(a) && Array.isArray(b)){
+      return a.reduce((sum, val) => sum + val, 0) - b.reduce((sum, val) => sum + val, 0);
+    }
+    if (Array.isArray(a)) {
+      return 1;
+    }
+    if (Array.isArray(b)) {
+      return -1;
+    }
+    return a-b;
+  })
+}
+console.log(sortArrayOfTask5(generate2dArrayX()));
+console.log('---------------------');
+
+console.log('7. attitional task, decode');
+const stringToDecode = '115!109!101!105!115!105!118!32!111!105!108!97!103!116!105!97!118!97!115!32!115!117!97!382!97!114!71!';
+const decode = (str) => {
+  const numbers = str.split(/!/g).reverse();
+  return String.fromCharCode(...numbers);
+}
+console.log(decode(stringToDecode));
+console.log('---------------------');
+
+
 document.write('<a href="../tasks/7.Multidimensional_arrays.pdf">Užduotis</a></br></br>');
