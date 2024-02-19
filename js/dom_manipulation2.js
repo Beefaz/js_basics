c3eb284c098bb25478e989771372344a8874ab8b
Beefaz <justinas.geryba@gmail.com>
2024-02-19 15:25:14 +0200

    task complete

--- js/dom_manipulation2.js
+++ js/dom_manipulation2.js
@@ -88,7 +88,7 @@ const createBall = (number) => {
   return ballWrapper;
 }
 
-const scrollTarget = document.getElementById('teleloto-breaker')
+const scrollTarget = document.getElementById('teleloto-breaker');
 const scrollToBottom = () => {
   scrollTarget.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 }
@@ -119,8 +119,8 @@ const ringTheBell = () => {
   bellDiv.classList.add('congratulate');
   ballContainer.append(bellDiv);
 
-  const gameover = new Audio('/assets/sounds/winner_bell.mp3');
-  gameover.play();
+  const gameOverSound = new Audio('/assets/sounds/winner_bell.mp3');
+  gameOverSound.play();
 
   setTimeout(() => {
     startBtn.style.display = 'none';
@@ -128,16 +128,16 @@ const ringTheBell = () => {
   }, 4000);
 }
 
+
 // ---------------------------------------------
 // square-game
-
 const squareGame = {
-  gameContainer:document.getElementById('square-hitter'),
-  statElement:document.getElementById('square-hitter-stats'),
-  roundsElement:document.getElementById('square-hitter-rounds'),
+  gameContainer: document.getElementById('square-hitter'),
+  statElement: document.getElementById('square-hitter-stats'),
+  roundsElement: document.getElementById('square-hitter-rounds'),
   userScore: 0,
   pcScore: 0,
-  maxScore: 5,
+  maxScore: 30,
   timers: [],
   rounds: [],
   round: 0,
@@ -149,9 +149,6 @@ const squareGame = {
   },
   addPcScore: function (score) {
     this.pcScore += score;
-  },
-  addMaxScore: function (score) {
-    this.maxScore += score;
   },
   addLevel: function () {
     this.level += 1;
@@ -165,7 +162,7 @@ const squareGame = {
   },
   checkGameEnd: function () {
     if (this.userScore >= this.maxScore || this.pcScore >= this.maxScore) {
-      if(this.round === 0) this.round++;
+      if (this.round === 0) this.round++;
       this.roundEnded = true;
       this.clearAllTimers();
       this.rounds.push({
@@ -211,7 +208,7 @@ const squareGame = {
   },
   updateRoundsUI: function () {
     let contentString = ''
-    this.rounds.forEach(({winner, round, level})=>{
+    this.rounds.forEach(({winner, round, level}) => {
       contentString += `<div class="round"> <div>Round: ${round} Level: ${level}</div><div>Winner: ${winner}</div></div>`
     })
     this.roundsElement.innerHTML = contentString;
@@ -237,9 +234,11 @@ const createNewBox = () => {
   let clicked = false;
   const currentTimer = window.setTimeout(() => {
     if (!clicked) {
+      squareGame.removeTimer(currentTimer);
+      const gameover = new Audio('/assets/sounds/error.mp3');
+      gameover.play();
       squareGame.addPcScore(1);
       squareGame.updateStatsUI();
-      squareGame.removeTimer(currentTimer);
       box.remove();
       createNewBox();
     }
@@ -249,8 +248,10 @@ const createNewBox = () => {
   //handleUserAction
   const boxClickHandler = (e) => {
     clicked = true;
-    e.target.remove();
     squareGame.removeTimer(currentTimer);
+    const clickSound = new Audio('/assets/sounds/click.mp3');
+    clickSound.play();
+    e.target.remove();
     squareGame.addUserScore(1);
     squareGame.updateStatsUI();
     createNewBox();
@@ -281,8 +282,298 @@ const decreaseGameLevel = () => {
 }
 
 const startNewRound = () => {
-  if (!squareGame.roundEnded) return undefined;
+  if (!squareGame.roundEnded) return;
+
   squareGame.gameContainer.innerHTML = '';
   squareGame.setupNextRound();
   createNewBox();
+}
+
+// ---------------------------------------------
+// google timer + stopwatch
+const timer = {
+  clockUI: document.getElementById('timer-ui'),
+  clockUIDisplay: document.getElementById('timer-ui-display'),
+  startBtn: document.getElementById('timer-start-btn'),
+  progressBar: document.getElementById('timer-progress-bar'),
+  timer: 300000,
+  timerDefault: 300000,
+  timers: [],
+  timeIsRunning: false,
+  handleClick: function () {
+    if (!this.timeIsRunning && this.timer > 0) {
+      this.startTimer();
+    } else {
+      this.pauseTimer();
+    }
+    this.updateStartBtnUI();
+  },
+  updateStartBtnUI: function () {
+    this.startBtn.innerHTML = this.timeIsRunning ? 'Stop' : 'Start';
+  },
+  calculateTime: function (ms) {
+    if (ms > 359999000) ms = 359999000;
+    let seconds = Math.floor((ms / 1000) % 60);
+    let minutes = Math.floor((ms / (1000 * 60)) % 60);
+    let hours = Math.floor((ms / (1000 * 60 * 60)));
+    return {hours, minutes, seconds};
+  },
+  setTimer: function (el) {
+    el.value = el.value.substring().replace(/[^0-9]/g, '');
+    let input = [...`${el.value}`].reverse().map((string) => parseInt(string));
+
+    this.clockUIDisplay.innerHTML = `
+      ${input[5] ?? 0}${input[4] ?? 0}<span>h</span>&nbsp
+      ${input[3] ?? 0}${input[2] ?? 0}<span>m</span>&nbsp
+      ${input[1] ?? 0}${input[0] ?? 0}<span>s</span>
+    `;
+
+    let miliseconds = 0;
+    if (input[0]) miliseconds += input[0] * 1000;
+    if (input[1]) miliseconds += input[1] * 10000;
+    if (input[2]) miliseconds += input[2] * 60000;
+    if (input[3]) miliseconds += input[3] * 600000;
+    if (input[4]) miliseconds += input[4] * 3600000;
+    if (input[5]) miliseconds += input[5] * 36000000;
+
+    this.timerDefault = this.timer = miliseconds;
+    this.pauseTimer();
+
+  },
+  startTimer: function () {
+    this.timeIsRunning = true;
+    const timer = window.setInterval(() => {
+      this.timer -= 1000;
+      this.updateTimerUI();
+      if (this.timer <= 0) {
+        this.pauseTimer();
+      }
+    }, 1000);
+    this.timers.push(timer);
+  },
+  pauseTimer() {
+    clearInterval(this.timers.pop());
+    this.timeIsRunning = false;
+    this.updateTimerUI();
+    this.updateStartBtnUI();
+  },
+  resetTimer: function () {
+    this.timer = this.timerDefault;
+    this.pauseTimer();
+    this.updateTimerUI();
+  },
+  updateTimerUI() {
+    const {hours, minutes, seconds} = this.calculateTime(this.timer);
+    this.clockUIDisplay.innerHTML = `
+      ${hours < 10 ? '0' + hours : hours}<span>h</span>&nbsp
+      ${minutes < 10 ? '0' + minutes : minutes}<span>m</span>&nbsp
+      ${seconds < 10 ? '0' + seconds : seconds}<span>s</span>
+    `;
+    if (this.progressBar) this.progressBar.style.background = `linear-gradient(to right, rgba(2,0,36,1) ${Math.round((this.timerDefault - this.timer) / this.timerDefault * 10000) / 100}%, rgba(0, 0, 0, 0) 0%)`;
+  },
+}
+
+//initiate timer
+timer.updateTimerUI();
+
+const stopwatch = {
+  clockUI: document.getElementById('stopwatch-ui'),
+  clockUIDisplay: document.getElementById('stopwatch-ui-display'),
+  startBtn: document.getElementById('stopwatch-start-btn'),
+  timer: 0,
+  timerDefault: 0,
+  timers: [],
+  timeIsRunning: false,
+  handleClick: function () {
+    if (!this.timeIsRunning) {
+      this.startTimer();
+    } else {
+      this.pauseTimer();
+    }
+    this.updateStartBtnUI();
+  },
+  updateStartBtnUI: function () {
+    this.startBtn.innerHTML = this.timeIsRunning ? 'Stop' : 'Start';
+  },
+  calculateTime: function (ms) {
+    let time = new Date(ms);
+    return {
+      hours: time.getUTCHours(),
+      minutes: time.getUTCMinutes(),
+      seconds: time.getUTCSeconds(),
+      milliseconds: time.getUTCMilliseconds()
+    }
+  },
+  resetTimer: function () {
+    this.timer = this.timerDefault;
+    this.pauseTimer();
+    this.updateStopwatchUi();
+  },
+  startTimer: function () {
+    this.timeIsRunning = true;
+    const timer = window.setInterval(() => {
+      this.timer += 10;
+      this.updateStopwatchUi();
+    }, 10);
+    this.timers.push(timer);
+  },
+  pauseTimer() {
+    clearInterval(this.timers.pop());
+    this.timeIsRunning = false;
+    this.updateStopwatchUi();
+    this.updateStartBtnUI();
+  },
+  updateStopwatchUi() {
+    const {hours, minutes, seconds, milliseconds} = this.calculateTime(this.timer);
+    let string = ''
+    if (hours > 0) string += `${hours < 10 ? '0' + hours : hours}<span>h</span>&nbsp`;
+    if (minutes > 0) string += `${hours < 10 ? '0' + hours : hours}<span>h</span>&nbsp`;
+    string += `${seconds < 10 ? '0' + seconds : seconds}<span>s</span>&nbsp<span>${milliseconds < 100 ? '0' + milliseconds / 10 : milliseconds / 10}</span>`
+    this.clockUIDisplay.innerHTML = string;
+  }
+}
+//initiate stopwatch
+stopwatch.updateStopwatchUi();
+
+const uiSwitchButtons = document.getElementsByClassName('clock-switcher')[0].children;
+const switchUI = (element) => {
+  if (element.classList.contains('active')) return;
+
+  const [button1, button2] = uiSwitchButtons;
+  button1.classList.toggle("active");
+  button2.classList.toggle("active");
+  timer.clockUI.classList.toggle("hidden");
+  stopwatch.clockUI.classList.toggle("hidden");
+};
+
+
+// ---------------------------------------------
+// autocomplete input
+const googleStrings = [
+  "What is my IP",
+  "How many weeks in a year",
+  "How many ounces in a cup",
+  "How to screenshot on Mac",
+  "When is the Super Bowl",
+  "When is Easter",
+  "When is Father's Day",
+  "What is Juneteenth",
+  "How do I register to vote",
+  "When is Thanksgiving",
+  "How many days until Christmas",
+  "How many ounces in a pound",
+  "How to tie a tie",
+  "How many liters in a gallon",
+  "How to screenshot on windows",
+  "How many cups in a quart",
+  "What holiday is today",
+  "How many oz in a gallon",
+  "How many quarts in a gallon",
+  "What is RSV",
+  "When is the next full moon",
+  "When is Mother's Day 2023",
+  "What is monkeypox",
+  "How long does covid last",
+  "How to delete apps",
+  "How to delete Instagram account",
+  "How many teaspoons in a tablespoon",
+  "When is Labor Day",
+  "When is Memorial Day",
+  "How many oz in a cup",
+  "How to lose weight fast",
+  "Where to vote",
+  "How many people are in the world",
+  "How to solve a Rubik's cube",
+  "How much house can I afford",
+  "How many feet in a mile",
+  "How long to boil eggs",
+  "What time is it in Australia",
+  "What is a recession",
+  "Why were chainsaws invented",
+  "What is pansexual",
+  "What does Let's go Brandon mean",
+  "When are taxes due 2023",
+  "How many steps in a mile",
+  "When does fall start",
+  "When does summer start",
+  "How to lower blood pressure",
+  "How to write a cover letter",
+  "How to take a screenshot on Mac",
+  "How to make French toast",
+  "What time does Walmart close",
+  "How old is Hasbulla",
+  "Why are flags at half mast today",
+  "What is a woman",
+  "How to take a screenshot",
+  "How to write a check",
+  "How many square feet in an acre",
+  "When is the next Powerball drawing",
+  "What time does McDonald's stop serving breakfast",
+  "How many grams in a pound",
+  "What does 444 mean",
+  "How many kids does Nick Cannon have",
+  "How long are you contagious with Covid",
+  "How to get rid of fruit flies",
+  "How many days in a year",
+  "What time is it in Arizona",
+  "How to delete Facebook account",
+  "How many ounces in a liter",
+  "How to take a screenshot on Windows",
+  "How did Jeffrey Dahmer die",
+  "What is a verb",
+  "Why is the sky blue",
+  "How to get rid of gnats",
+  "How old is Jenna Ortega",
+  "When is Christmas",
+  "How old is Dolly Parton",
+  "How many seconds in a day",
+  "When is Mardi Gras 2023",
+  "Where is the Super Bowl this year",
+  "How many continents are there",
+  "What time is it in Germany",
+  "What channel is the Super Bowl on",
+  "How did Elvis die",
+  "How many tablespoons in 1/4 cup",
+  "What time is it in India",
+  "How to get rid of hiccups",
+  "What does 222 mean",
+  "How old is Tom Cruise",
+  "How long does it take to get a passport",
+  "What is MS",
+  "How to screenshot on Chromebook",
+  "What does SOS mean",
+  "What does woke mean",
+  "How to say hi in Spanish",
+  "What is gaslighting",
+  "What does monkeypox look like",
+  "How many countries are there",
+  "How old is Queen Elizabeth",
+  "How many genders are there",
+  "What does rizz mean"
+]
+
+const handleInput = {
+  input: document.getElementById('autocomplete-input'),
+  options: document.getElementById('autocomplete-options'),
+  getOptions: function (el) {
+    const filteredMappedElements =
+      googleStrings
+        .filter((sentence) =>
+          sentence.toUpperCase().includes(el.value.toUpperCase()))
+        .sort((a, b) => a.toUpperCase() - b.toUpperCase())
+        .slice(0, 5)
+        .map((string) => this.createOptionElement(string));
+    this.options.replaceChildren(...filteredMappedElements);
+  },
+  selectOption: function (string) {
+    this.input.value = string;
+    this.options.innerHTML = '';
+  },
+  createOptionElement: function (string) {
+    const element = document.createElement('div');
+    element.className = 'input-option';
+    element.innerHTML = string;
+    element.onclick = () => this.selectOption(string);
+    return element;
+  }
 }
